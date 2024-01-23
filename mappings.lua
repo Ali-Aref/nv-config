a025509b25a796ab0f8556346b643015dc6b0307
Ali Aref <aliaref.js@gmail.com>
2024-01-23 09:25:31 +0430

    added: macos mappings

--- mappings.lua
+++ mappings.lua
@@ -15,6 +15,25 @@ M.general = {
     ["<A-k>"] = { "<C-w>k", "window up" },
     -- toggle sidebar
     ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>" },
+    -- term
+    ["<leader>tt"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+    ["<leader>h"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+   ["<leader>v"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
   },
 }
 
@@ -23,8 +42,32 @@ M.whichkey = {
   n = {
     -- rest client
     ["<leader>rr"] = { "<Plug>RestNvim", "run restapi request" },
-    ["<leader>rl"] = { "<Plug>RestNvimLast", "rerun last api request"  },
+    ["<leader>rl"] = { "<Plug>RestNvimLast", "rerun last api request" },
     ["<leader>rv"] = { "<Plug>RestNvimPreview", "perview restapi curl" },
+  },
+}
+
+-- for macos
+M.nvterm = {
+  t = {
+    ["<leader>tt"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+    ["<leader>h"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
+    ["<leader>v"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "Toggle vertical term",
+    },
   },
 }
 
