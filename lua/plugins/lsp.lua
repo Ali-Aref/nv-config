582b8808eefe6cda6b535214dd48556eb325d528
Ali Aref <aliaref.js@gmail.com>
2024-04-09 21:50:36 +0430

    up: loader.from_vscode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,12 +7,7 @@ return {
 		"L3MON4D3/LuaSnip",
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
-			require("luasnip/loaders/from_vscode").lazy_load({
-				paths = "/home/ali/.local/share/nvim/lazy/friendly-snippets/"
-			})
-			require("luasnip.loaders.from_vscode").lazy_load({
-				paths = "/home/ali/.local/share/nvim/lazy/friendly-snippets/"
-			})
+			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
 }
