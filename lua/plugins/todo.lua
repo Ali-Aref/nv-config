a8a716bf1a33b1c2e6d58a30df4a65943df87fa3
Ali Aref <aliaref.js@gmail.com>
2024-04-14 15:05:20 +0430

    add: todo-comments

---
+++ lua/plugins/todo.lua
@@ -0,0 +1,24 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = { "TodoQuickFix", "TodoLocList", "TodoTrouble", "TodoTelescope" },
+	opts = {
+		keywords = {
+			FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+			WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+			HACK = { icon = "", color = "warning" },
+			TEST = { icon = "󰙨", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			PERF = { icon = "", color = "ref", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			TODO = { icon = "", color = "info" },
+			NOTE = { icon = "", color = "hint", alt = { "INFO", "START" } },
+		},
+		colors = {
+			info = { "#25afff" },
+			warning = { "#fc8f59" },
+			error = { "#ff5050" },
+			test = { "#D03259" },
+			ref = { "#7084c6" },
+			hint = { "#10B981" },
+		},
+	},
+}
