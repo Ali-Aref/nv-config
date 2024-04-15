7dba7bb2e03a847729ffa42cd1e81b80cada9c21
Ali Aref <aliaref.js@gmail.com>
2024-04-15 06:38:45 +0430

    up: lsp-icons, added actions

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,8 +4,12 @@ lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
--- for lanugage server check
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+lsp_zero.set_sign_icons({
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
+})
 
 -- auto complete
 local cmp = require("cmp")
@@ -32,8 +36,34 @@ cmp.setup({
 	formatting = require("lsp-zero").cmp_format({ details = true }),
 })
 
+-- lsp mappings
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+		vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+		vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+		vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+	end,
+})
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- for lanugage server check
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
 -- tsserver
 require("lspconfig").tsserver.setup({
+	capabilities = lsp_capabilities,
 	filetypes = {
 		"javascript",
 		"typescript",
@@ -43,10 +73,10 @@ require("lspconfig").tsserver.setup({
 })
 
 -- pyright
-require("lspconfig").pyright.setup({})
+require("lspconfig").pyright.setup({ capabilities = lsp_capabilities })
 
 -- vscode-langservers-extracted
-require("lspconfig").eslint.setup({})
-require("lspconfig").jsonls.setup({})
-require("lspconfig").cssls.setup({})
-require("lspconfig").html.setup({})
+require("lspconfig").eslint.setup({ capabilities = lsp_capabilities })
+require("lspconfig").jsonls.setup({ capabilities = lsp_capabilities })
+require("lspconfig").cssls.setup({ capabilities = lsp_capabilities })
+require("lspconfig").html.setup({ capabilities = lsp_capabilities })
