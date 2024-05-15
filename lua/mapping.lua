a031ec0ce93fe487a5d59d3f47dac922ff30a845
Ali Aref <aliaref.js@gmail.com>
2024-05-15 04:45:43 +0430

    add: resize pane binds

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -20,6 +20,13 @@ vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Move right" })
 vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Move up" })
 vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Move down" })
 
+-- resize panes
+vim.keymap.set("n", "<leader>h", "<cmd>vertical resize -10<CR>", { desc = "Shrink pane vertically" }) 
+vim.keymap.set("n", "<leader>l", "<cmd>vertical resize +10<CR>", { desc = "Gorw pane vertically" })
+vim.keymap.set("n", "<leader>j", "<cmd>resize +5<CR>", { desc = "Grow pane horizontally" })
+vim.keymap.set("n", "<leader>k", "<cmd>resize -5<CR>", { desc = "Shrink pane horizontally" })
+
+
 -- conform
 vim.keymap.set({ "n", "v" }, "<leader>fm", function()
 	require("conform").format({
@@ -46,4 +53,5 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Files" 
 -- bufferline
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "To next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "To prev buffer" })
+
 
