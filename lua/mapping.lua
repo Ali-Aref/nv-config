bff6b1c6108bb18b513c32e88757ac86f2b1f9d9
Ali Aref <aliaref.js@gmail.com>
2024-06-23 16:09:09 +0430

    added(mapping): for global search

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -21,11 +21,10 @@ vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Move up" })
 vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Move down" })
 
 -- resize panes
-vim.keymap.set("n", "<leader>h", "<cmd>vertical resize -10<CR>", { desc = "Shrink pane vertically" }) 
+vim.keymap.set("n", "<leader>h", "<cmd>vertical resize -10<CR>", { desc = "Shrink pane vertically" })
 vim.keymap.set("n", "<leader>l", "<cmd>vertical resize +10<CR>", { desc = "Gorw pane vertically" })
 vim.keymap.set("n", "<leader>j", "<cmd>resize +5<CR>", { desc = "Grow pane horizontally" })
 vim.keymap.set("n", "<leader>k", "<cmd>resize -5<CR>", { desc = "Shrink pane horizontally" })
-
 
 -- conform
 vim.keymap.set({ "n", "v" }, "<leader>fm", function()
@@ -40,13 +39,16 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>fa", builtin.git_files, { desc = "Find all files" })
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Live grep " })
-vim.keymap.set("n", "<leader>fg", builtin.grep_string, { desc = "Grep string under cursor" })
+vim.keymap.set("n", "<leader>fW", builtin.grep_string, { desc = "Grep string under cursor" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
 vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "Choose color scheme" })
 --lsp
 vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find LSP reference from under cursor" })
 vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find LSP document symbols" })
 vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Find LSP workspace symbols" })
+--global search and replace
+vim.keymap.set("n", "<leader>fgs", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre", })
+vim.keymap.set("n", "<leader>fgw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', { desc = "Search current word", })
 
 -- nvim tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Files" })
@@ -54,5 +56,3 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Files" 
 -- bufferline
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "To next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "To prev buffer" })
-
-
