ea3c8c57c61cb62207b1fb94a13e7e288a632ceb
Ali Aref <aliaref.js@gmail.com>
2024-08-15 12:00:05 +0430

    update(plugins): to latest version

--- init.lua
+++ init.lua
@@ -24,5 +24,5 @@ require("luasnip").filetype_extend("python", { "django" })
 -- theme
 -- vim.cmd("colorscheme nord")
 -- vim.o.background = "dark" -- dark/light gruvbox dark mode
-vim.cmd("colorscheme tokyonight-night")
--- vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme gruvbox")
