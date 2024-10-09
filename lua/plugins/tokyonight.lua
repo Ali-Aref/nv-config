72a2e0faf0008add6e08bbc1ea5034cc490e197e
Ali Aref <aliaref.js@gmail.com>
2024-10-09 10:02:52 +0000

    update(line-numbers): colors to be visible in transparent mode

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -16,8 +16,8 @@ return {
 				functions = {},
 				variables = {},
 				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
+				sidebars = "transparent", -- style for sidebars, see below
+				floats = "transparent", -- style for floating windows
 			},
 			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
 			dim_inactive = false, -- dims inactive windows
@@ -26,13 +26,20 @@ return {
 			--- You can override specific color groups to use other groups or a hex color
 			--- function will be called with a ColorScheme table
 			---@param colors ColorScheme
-			on_colors = function(colors) end,
+			on_colors = function(colors)
+				-- colors.border = colors.dark3
+			end,
 
 			--- You can override specific highlights to use other groups or a hex color
 			--- function will be called with a Highlights and ColorScheme table
 			---@param highlights tokyonight.Highlights
 			---@param colors ColorScheme
-			on_highlights = function(highlights, colors) end,
+			on_highlights = function(highlights, colors)
+					highlights.LineNr = { fg = colors.dark5 }  
+					highlights.CursorLineNr = { fg = colors.green, bold = true }
+					highlights.LineNrAbove = { fg = colors.dark5 }  
+					highlights.LineNrBelow = { fg = colors.dark5 } 
+			end,
 
 			cache = true, -- When set to true, the theme will be cached for better performance
 
