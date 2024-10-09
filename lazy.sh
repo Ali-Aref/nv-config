8552695f294c209a12346a50cf8401644b0d9178
Ali Aref <aliaref.js@gmail.com>
2024-10-09 10:04:06 +0000

    add(scripting): to install the language servers

--- lazy.sh
+++ lazy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # run this script only if you don't have the ~/.local/share/nvim/ directory
-git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy/
+# git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy/
+
+# installing lsp
+# ts_ls
+npm install -g typescript custom-elements-languageserver
+# tailwind
+npm install -g @tailwindcss/language-server
+# vscode language servers
+# eslint, jsonls, cssls, html, ...
+npm i -g vscode-langservers-extracted
