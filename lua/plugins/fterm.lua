c31277e1ff0ab742ae14642c7ff2ee9513f3b366
Ali Aref <aliaref.js@gmail.com>
2024-11-14 09:09:49 +0430

    add(plugin): fterm

---
+++ lua/plugins/fterm.lua
@@ -0,0 +1,47 @@
+return {
+	"numToStr/FTerm.nvim",
+	-- lazy = true,
+	config = function()
+		local fterm = require("FTerm")
+		fterm.setup({
+			dimensions = {
+				height = 0.6,
+				width = 0.6,
+				-- x = 0.5,
+				-- y = 0.5,
+			},
+		})
+
+		local runners = { lua = "lua", javascript = "node", python = "python" }
+
+		local lazygit = fterm:new({
+			cmd = "lazygit",
+			dimensions = { height = 0.9, width = 0.9 },
+		})
+		local bottomTerm = fterm:new({
+			dimensions = { height = 0.4, width = 1, x = 0, y = 1 },
+		})
+
+		-- floating
+		vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle terminal" })
+		vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle terminal" })
+		-- lazygit
+		vim.keymap.set("n", "<A-g>", function() lazygit:toggle() end, { desc = "lazygit" })
+		vim.keymap.set("t", "<A-g>", function() lazygit:toggle() end, { desc = "Toggle lazygit" })
+		-- bottom terminal
+		vim.keymap.set("n", "<A-\\>", function() bottomTerm:toggle() end, { desc = "Toggle bottom terminal" })
+		vim.keymap.set("t", "<A-\\>", function() bottomTerm:toggle() end, { desc = "Toggle bottom terminal" })
+		-- run
+		vim.keymap.set("n", "<A-r>", function()
+			local buf = vim.api.nvim_buf_get_name(0)
+			local ftype = vim.filetype.match({ filename = buf })
+			local exec = runners[ftype]
+			if exec ~= nil then
+				require("FTerm").scratch({
+					cmd = { exec, buf },
+					dimensions = { height = 0.3, width = 1, x = 0, y = 1 }
+				})
+			end
+		end, { desc = "Run file" })
+	end,
+}
