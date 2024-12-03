3e9dd82716c6b3c27eed5f6ead6c54f581e4bba5
Ali Aref <aliaref.js@gmail.com>
2024-12-03 09:41:06 +0430

    add(plugin): git_blame

---
+++ lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+	-- for lualine
+	-- {
+	-- 	"f-person/git-blame.nvim",
+	-- 	event = "VeryLazy",
+	-- 	opts = {
+	-- 		-- your configuration comes here
+	-- 		enabled = true,
+	-- 		display_virtual_text = false,
+	-- 		-- available template options
+	-- 		-- <author> <committer> <date> <committer-date> <summary> <sha>
+	-- 		message_template = "<summary> - <date>",
+	-- 		-- date_format = "%m-%d-%Y %H:%M:%S",
+	-- 		date_format = "%a, %H:%M %d:%m:%Y ",
+	-- 		virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+	-- 		schedule_event = "CursorMoved", -- CursorMoved|CursorHold
+	-- 		clear_event = "CursorMovedI", -- CursorMovedI|CursorHoldI
+	-- 	},
+	-- },
+}
